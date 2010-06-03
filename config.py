14d7e352f7cd36a41b438ab42c61cdb6a0c047b3
jonathan <jonathan@qoda-server.(none)>
2010-06-03 02:01:12 +0200

    added stats file global var

--- config.py
+++ config.py
@@ -3,9 +3,12 @@ Global config file. Change variable below as needed but ensure that the log and
 retry files have the correct permissions.
 """
 
+from datetime import datetime
+
 # file settings
 LOG_FILENAME = '/tmp/pymailer.log'
 CSV_RETRY_FILENAME = '/tmp/pymailer.csv'
+STATS_FILE = '/tmp/pymailer-%s.stat' % str(datetime.now()).replace(' ', '-').replace(':', '-').replace('.', '-')
 
 # smtp settings
 SMTP_HOST = 'localhost'
