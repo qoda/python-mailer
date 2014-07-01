07d6757a2653faa01ced6af6d783dd6c69f7fea1
qoda <jonathan.bydendyk@is.co.za>
2014-07-01 14:46:12 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pymailer',
-    version='dev',
+    version='0.1.0',
     description='Simple python bulk mailer script.',
     author='Jonathan Bydendyk',
     author_email='jonathan@blu-marble.co.za',
